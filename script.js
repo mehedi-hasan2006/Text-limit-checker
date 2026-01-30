d7ce8ac4097cc6171c70b1ab7181fc294ac16b36
mehedi-hasan2006 <mehedihasan811914@gmail.com>
2026-01-31 00:12:47 +0600

    JavaScript Function Added

---
+++ script.js
@@ -0,0 +1,27 @@
+let text = document.getElementById("textarea");
+let limitChecker = document.getElementById("limitChecker");
+
+let limit = 10;
+
+let result = 0 + "/" + limit;
+limitChecker.textContent = result;
+
+text.addEventListener("input", () => {
+  let count = textarea.value.length;
+
+  limitChecker.textContent = count + "/" + limit;
+
+  if (count > limit) {
+    text.classList.remove("focus:ring-violet-600");
+    text.classList.remove("border-none");
+    text.classList.add("focus:ring-red-600");
+
+    limitChecker.style.color = "red";
+  } else {
+    text.classList.add("focus:ring-2");
+    text.classList.add("focus:ring-violet-600");
+    text.classList.add("focus:outline-none");
+
+    limitChecker.style.color = "#292929";
+  }
+});
